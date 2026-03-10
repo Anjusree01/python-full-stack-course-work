a8345f3f3aad8698cbf98f55ec524e5925126068
Anjus <anjus@example.com>
2026-03-11 00:18:19 +0530

    Import local workspace

---
+++ practice.py
@@ -0,0 +1,13 @@
+
+a = 10
+b = 3
+
+print("1. ARITHMETIC OPERATORS")
+print(f"a = {a}, b = {b}")
+print(f"a + b  = {a + b}")    # Addition
+print(f"a - b  = {a - b}")    # Subtraction
+print(f"a * b  = {a * b}")    # Multiplication
+print(f"a / b  = {a / b}")    # Division
+print(f"a // b = {a // b}")   # Floor Division
+print(f"a % b  = {a % b}")    # Modulus (remainder)
+print(f"a ** b = {a ** b}")   # Exponent
